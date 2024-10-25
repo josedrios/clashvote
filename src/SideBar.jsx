f15ac3de839421e2832011a2cc8dde17479875f3
Jose Rios <joserios0531@gmail.com>
2024-10-24 22:09:17 -0700

    Added spacing for sidebar links and made hover effect for side bar link

--- src/SideBar.jsx
+++ src/SideBar.jsx
@@ -4,12 +4,14 @@ function SideBar() {
   return (
     <aside id = 'main-sidebar'>
         <div className='sidebar-section' id='top-sidebar'>
-            <a className='sidebar-link'>Current Season</a>
-            <a className='sidebar-link'>Player Search</a>
+              <a className='sidebar-link'>Current Season</a>
+              <a className='sidebar-link'>Player Search</a>
         </div>
+
         <div className='sidebar-section' id='mid-sidebar'>
-            <a className='sidebar-link'> Season 82</a>
+            <a className='sidebar-link'>Season 82</a>
         </div>
+        
         <div className='sidebar-section' id='bottom-sidebar'>
             <a className='sidebar-link'>About</a>
             <a className='sidebar-link'>Contact</a>
