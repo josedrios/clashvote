daaa2d3eaf7b81f8846f21e850100815ba20f696
Jose Rios <joserios0531@gmail.com>
2024-10-25 18:14:09 -0700

    Re-designed logo/website name for navbar, made it more simple

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,7 +4,7 @@ function Navbar() {
   return (
     <nav id = 'main-navbar'>
         <div id='website-name-container'>
-          <h1 id='website-name'><span>cocvote</span>.com</h1>
+          <h1 id='website-name'><span>cocvote</span>.</h1>
         </div>
     </nav>
   )
