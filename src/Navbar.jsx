710b72c9183ebd88bd881a16660642c6b5e5809b
Jose Rios <joserios0531@gmail.com>
2024-10-25 18:11:59 -0700

    Added sass to website name on the navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -3,7 +3,9 @@ import { useState } from 'react'
 function Navbar() {
   return (
     <nav id = 'main-navbar'>
-        Navbar
+        <div id='website-name-container'>
+          <h1 id='website-name'><span>cocvote</span>.com</h1>
+        </div>
     </nav>
   )
 }
