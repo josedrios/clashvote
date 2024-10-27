d1d21161284676580b92f97a1faf9800b8e2714c
Jose Rios <joserios0531@gmail.com>
2024-10-26 17:08:56 -0700

    Made progress with horizontal bar graph but issue with showing respective percentages for each bar, work in progress

--- src/Season.jsx
+++ src/Season.jsx
@@ -1,6 +1,11 @@
 import { useState } from 'react'
 import { IoMdArrowDropdown } from "react-icons/io";
 
+import aq from './assets/images/archer_queen.png';
+import bk from './assets/images/barb_king.png';
+import gw from './assets/images/grand_warden.png';
+import rc from './assets/images/royal_champ.png';
+
 function Season() {
   return (
     <div id='season-container'>
@@ -20,34 +25,34 @@ function Season() {
             <div className='horizontal-bar-graph-container'>
                 <div className='axis-section'>
                     <div className='axis-section-header'>
-                        
+                        <img className='axis-image' src={aq} alt="" />
                     </div>
                     <div className='test' id='queen-test'>
-                    
+                        <h3 className='axis-percentage'>60%</h3>
                     </div>
                 </div>
                 <div className='axis-section'>
                     <div className='axis-section-header'>
-                        
+                        <img className='axis-image' src={bk} alt="" />
                     </div>
                     <div className='test' id='king-test'>
-                    
+                        <h3 className='axis-percentage'>50%</h3>
                     </div>
                 </div>
                 <div className='axis-section'>
                     <div className='axis-section-header'>
-                        
+                        <img className='axis-image' src={gw} alt="" />
                     </div>
                     <div className='test' id='gw-test'>
-                    
+                        <h3 className='axis-percentage'>10%</h3>
                     </div>
                 </div>
                 <div className='axis-section'>
                     <div className='axis-section-header'>
-                        
+                        <img className='axis-image' src={rc} alt="" />
                     </div>
                     <div className='test' id='rc-test'>
-                    
+                        <h3 className='axis-percentage'>0%</h3>
                     </div>
                 </div>
             </div>
