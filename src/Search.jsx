40a7bb9f6696454336565da6797768345dcef4e4
Jose Rios <joserios0531@gmail.com>
2024-10-31 22:47:12 -0700

    Put searchbar in a form, added a button for submission as well, made a handler function for the form to prevent defeault and update user entry state, added temporary h1 tag to view state

--- src/Search.jsx
+++ src/Search.jsx
@@ -2,12 +2,34 @@ import { useState } from 'react'
 import './styles/main.scss'
 import SearchInfo from './SearchInfo'
 import SearchResult from './SearchResult'
-
+import { IoIosSearch } from "react-icons/io";
 
 function Search() {
+  const [userEntry, setUserEntry] = useState('');
+
+  function handleFormSubmit(event) {
+    event.preventDefault();
+    const userInput = document.getElementById('searchbar').value;
+    setUserEntry(userInput);
+  }
+
   return (
     <div id='search-page-container'>
-        <input id='searchbar' type="text" placeholder='Enter tag'/>
+        <form  id='searchbar-container' onSubmit={handleFormSubmit}>
+          <div id='searchbar-toggle'>
+            TOGGLE
+          </div>
+          <input id='searchbar' type="text" placeholder='Enter tag'/>
+          <button type='submit' id='searchbar-submit-button'>
+            <IoIosSearch />
+          </button>
+        </form>
+        <h1 
+          style={{
+            color: "white"
+          }}>
+          USER ENTRY STATE: {userEntry}
+        </h1>
         {/* <SearchInfo/> */}
         <SearchResult/>
     </div>
