71ea3248b1b291a56d897b508464dd2ece75b000
josedrios <joserios0531@gmail.com>
2024-11-11 15:09:24 -0800

    Added remainder of missing images, styled troop image and level displayment

--- src/Components/feature/Images.js
+++ src/Components/feature/Images.js
@@ -1,24 +1,33 @@
-import Archer_Queen from '../../assets/images/Archer_Queen.png';
+import Angry_Jelly from '../../assets/images/Angry_Jelly.png';
+import Apprentice_Warden from '../../assets/images/Apprentice_Warden.png';
 import Archer from '../../assets/images/Archer.png';
+import Archer_Queen from '../../assets/images/Archer_Queen.png';
 import Baby_Dragon from '../../assets/images/Baby_Dragon.png';
 import Balloon from '../../assets/images/Balloon.png';
-import Barbarian_King from '../../assets/images/Barbarian_King.png';
 import Barbarian from '../../assets/images/Barbarian.png';
+import Barbarian_King from '../../assets/images/Barbarian_King.png';
 import Bat_Spell from '../../assets/images/Bat_Spell.png';
 import Battle_Blimp from '../../assets/images/Battle_Blimp.png';
 import Battle_Copter from '../../assets/images/Battle_Copter.png';
+import Battle_Drill from '../../assets/images/Battle_Drill.png';
 import Battle_Machine from '../../assets/images/Battle_Machine.png';
 import Beta_Minion from '../../assets/images/Beta_Minion.png';
 import Bomber from '../../assets/images/Bomber.png';
 import Bowler from '../../assets/images/Bowler.png';
 import Boxer_Giant from '../../assets/images/Boxer_Giant.png';
 import Cannon_Cart from '../../assets/images/Cannon_Cart.png';
-import Dragon_Rider from '../../assets/images/Dragon_Rider.png';
+import Diggy from '../../assets/images/Diggy.png';
 import Dragon from '../../assets/images/Dragon.png';
+import Dragon_Rider from '../../assets/images/Dragon_Rider.png';
+import Druid from '../../assets/images/Druid.png';
 import Drop_Ship from '../../assets/images/Drop_Ship.png';
 import Electro_Dragon from '../../assets/images/Electro_Dragon.png';
+import Electro_Owl from '../../assets/images/Electro_Owl.png';
+import Electro_Titan from '../../assets/images/Electro_Titan.png';
+import Electrofire_Wizard from '../../assets/images/Electrofire_Wizard.png';
 import Flame_Flinger from '../../assets/images/Flame_Flinger.png';
 import Freeze_Spell from '../../assets/images/Freeze_Spell.png';
+import Frosty from '../../assets/images/Frosty.png';
 import Giant from '../../assets/images/Giant.png';
 import Goblin from '../../assets/images/Goblin.png';
 import Golem from '../../assets/images/Golem.png';
@@ -34,24 +43,30 @@ import Ice_Hound from '../../assets/images/Ice_Hound.png';
 import Inferno_Dragon from '../../assets/images/Inferno_Dragon.png';
 import Invisibility_Spell from '../../assets/images/Invisibility_Spell.png';
 import Jump_Spell from '../../assets/images/Jump_Spell.png';
+import L_A_S_S_I from '../../assets/images/L.A.S.S.I.png';
 import Lava_Hound from '../../assets/images/Lava_Hound.png';
 import Lightning_Spell from '../../assets/images/Lightning_Spell.png';
 import Log_Launcher from '../../assets/images/Log_Launcher.png';
+import Mighty_Yak from '../../assets/images/Mighty_Yak.png';
 import Miner from '../../assets/images/Miner.png';
 import Minion from '../../assets/images/Minion.png';
 import Night_Witch from '../../assets/images/Night_Witch.png';
 import P_E_K_K_A from '../../assets/images/P.E.K.K.A.png';
+import Phoenix from '../../assets/images/Phoenix.png';
+import Poison_Lizard from '../../assets/images/Poison_Lizard.png';
 import Poison_Spell from '../../assets/images/Poison_Spell.png';
 import Power_P_E_K_K_A from '../../assets/images/Power_P.E.K.K.A.png';
 import Rage_Spell from '../../assets/images/Rage_Spell.png';
 import Raged_Barbarian from '../../assets/images/Raged_Barbarian.png';
 import Recall_Spell from '../../assets/images/Recall_Spell.png';
 import Rocket_Balloon from '../../assets/images/Rocket_Balloon.png';
+import Root_Rider from '../../assets/images/Root_Rider.png';
 import Royal_Champion from '../../assets/images/Royal_Champion.png';
 import Siege_Barracks from '../../assets/images/Siege_Barracks.png';
 import Skeleton_Spell from '../../assets/images/Skeleton_Spell.png';
 import Sneaky_Archer from '../../assets/images/Sneaky_Archer.png';
 import Sneaky_Goblin from '../../assets/images/Sneaky_Goblin.png';
+import Spirit_Fox from '../../assets/images/Spirit_Fox.png';
 import Stone_Slammer from '../../assets/images/Stone_Slammer.png';
 import Super_Archer from '../../assets/images/Super_Archer.png';
 import Super_Barbarian from '../../assets/images/Super_Barbarian.png';
@@ -65,6 +80,7 @@ import Super_Valkyrie from '../../assets/images/Super_Valkyrie.png';
 import Super_Wall_Breaker from '../../assets/images/Super_Wall_Breaker.png';
 import Super_Witch from '../../assets/images/Super_Witch.png';
 import Super_Wizard from '../../assets/images/Super_Wizard.png';
+import Unicorn from '../../assets/images/Unicorn.png';
 import Valkyrie from '../../assets/images/Valkyrie.png';
 import Wall_Breaker from '../../assets/images/Wall_Breaker.png';
 import Wall_Wrecker from '../../assets/images/Wall_Wrecker.png';
@@ -73,27 +89,36 @@ import Wizard from '../../assets/images/Wizard.png';
 import Yeti from '../../assets/images/Yeti.png';
 
 const images = {
-    Archer_Queen,
+    Angry_Jelly,
+    Apprentice_Warden,
     Archer,
+    Archer_Queen,
     Baby_Dragon,
     Balloon,
-    Barbarian_King,
     Barbarian,
+    Barbarian_King,
     Bat_Spell,
     Battle_Blimp,
     Battle_Copter,
+    Battle_Drill,
     Battle_Machine,
     Beta_Minion,
     Bomber,
     Bowler,
     Boxer_Giant,
     Cannon_Cart,
-    Dragon_Rider,
+    Diggy,
     Dragon,
+    Dragon_Rider,
+    Druid,
     Drop_Ship,
     Electro_Dragon,
+    Electro_Owl,
+    Electro_Titan,
+    Electrofire_Wizard,
     Flame_Flinger,
     Freeze_Spell,
+    Frosty,
     Giant,
     Goblin,
     Golem,
@@ -109,24 +134,30 @@ const images = {
     Inferno_Dragon,
     Invisibility_Spell,
     Jump_Spell,
+    L_A_S_S_I,
     Lava_Hound,
     Lightning_Spell,
     Log_Launcher,
+    Mighty_Yak,
     Miner,
     Minion,
     Night_Witch,
     P_E_K_K_A,
+    Phoenix,
+    Poison_Lizard,
     Poison_Spell,
     Power_P_E_K_K_A,
     Rage_Spell,
     Raged_Barbarian,
     Recall_Spell,
     Rocket_Balloon,
+    Root_Rider,
     Royal_Champion,
     Siege_Barracks,
     Skeleton_Spell,
     Sneaky_Archer,
     Sneaky_Goblin,
+    Spirit_Fox,
     Stone_Slammer,
     Super_Archer,
     Super_Barbarian,
@@ -140,12 +171,14 @@ const images = {
     Super_Wall_Breaker,
     Super_Witch,
     Super_Wizard,
+    Unicorn,
     Valkyrie,
     Wall_Breaker,
     Wall_Wrecker,
     Witch,
     Wizard,
-    Yeti,
+    Yeti
 };
 
 export default images;
+
--- src/Components/feature/PlayerHome.jsx
+++ src/Components/feature/PlayerHome.jsx
@@ -6,26 +6,22 @@ function PlayerHome({playerHome}) {
         return (
         <div id='player-home-data'>
             <h3>HOME VILLAGE TROOPS:</h3>
-            <div>
+            <div className='troops-container' id='player-home-troops'>
             {playerHome.troops.map((troop, index) => (
                 troop.village === 'home' ? (
                     <div className='troop-container' key={index}>
-                        <h4>{troop.name}: {troop.level},{troop.village}</h4>
-                        <h4>TEST: {troop.name.replace(/[ .]/g, '_')}.png</h4>
-
+                        <h4 className='troop-level'>{troop.level}</h4>
                         <img id='test-img' src={getImage(troop.name)} alt="" />                   
                     </div>
                 ) : null
             ))}
             </div>
             <h3>BUILDER VILLAGE TROOPS:</h3>
-            <div>
+            <div className='troops-container' id='player-home-troops'>
             {playerHome.troops.map((troop, index) => (
                 troop.village === 'builderBase' ? (
                     <div className='troop-container' key={index}>
-                        <h4>{troop.name}: {troop.level},{troop.village}</h4>
-                        <h4>TEST: {troop.name.replace(/[ .]/g, '_')}.png</h4>
-
+                        <h4 className='troop-level'>{troop.level}</h4>
                         <img id='test-img' src={getImage(troop.name)} alt="" />                   
                     </div>
                 ) : null
