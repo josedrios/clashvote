8f671d18e9538dba7e71d547328f5b69ba0a0ac2
josedrios <joserios0531@gmail.com>
2024-12-27 18:14:37 -0800

    Fully fixed sidebar resizing bug, also improved sidebar toggle (now toggles when it comes to clicking on navbar)

--- src/Components/layout/Navbar.jsx
+++ src/Components/layout/Navbar.jsx
@@ -6,7 +6,7 @@ import { HiBars2 } from "react-icons/hi2";
 
 function Navbar({isSideBarOpen, toggleSideBar, changeMainView}) {
   return (
-    <nav id = 'main-navbar'>
+    <nav id = 'main-navbar' onClick={toggleSideBar}>
         <button onClick={toggleSideBar} id='bars-icon-button' aria-expanded={isSideBarOpen} aria-controls='#main-sidebar'>
           <svg id='bars-icon' viewBox='0 0 100 100' width={40} height={40}>
             <rect id='top-bar' className='bars' width={70} height={10} x={10} y={27} rx={5}></rect>
