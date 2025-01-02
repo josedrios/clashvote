0274f75247a9d0703991eebcc31ae11803cf0784
josedrios <joserios0531@gmail.com>
2025-01-02 13:47:02 -0800

    Added heroes respective equiments onto the frontend

--- src/Components/feature/PlayerBase.jsx
+++ src/Components/feature/PlayerBase.jsx
@@ -19,40 +19,76 @@ function PlayerHome({ base, trophies }) {
                         {base.base === "home" ? "Home Village" : "Builder Base"}
                     </h3>
                     <div className="player-base-header-trophies">
-                        <p>
-                            Current:{trophies.current}
-                        </p>
-                        <GoTrophy className="trophy-icon"/>
-                        /
-                        <p>
-                            Best:{trophies.best}
-                        </p>
+                        <p>Current:{trophies.current}</p>
+                        <GoTrophy className="trophy-icon" />/
+                        <p>Best:{trophies.best}</p>
                         <GoTrophy className="trophy-icon" />
                     </div>
                 </div>
                 <img className="testing-img" src={getImage("th14")} alt="" />
             </div>
-            <div className="troop-section">
+            {base.heroes && (
+                <div className="troop-section">
                 <h3>HEROES</h3>
                 <div className="troops-container">
                     {base.heroes.map((troop, index) => (
-                        <div className="troop-container" key={index}>
-                            <h4 className={`troop-level ${troop.level === troop.maxLevel ? 'max-level' : ''}`}>{troop.level}</h4>
-                            <img
-                                className="unit-img"
-                                src={getImage(troop.name)}
-                                alt=""
-                            />
+                        <div>
+                            <div
+                                className="troop-container hero-troop-container"
+                                key={index}
+                            >
+                                <h4
+                                    className={`troop-level ${
+                                        troop.level === troop.maxLevel
+                                            ? "max-level"
+                                            : ""
+                                    }`}
+                                >
+                                    {troop.level}
+                                </h4>
+                                <img
+                                    className="unit-img"
+                                    src={getImage(troop.name)}
+                                    alt=""
+                                />
+                            </div>
+                            {troop.equipment?.[0] && (
+                                <div className="hero-equipment">
+                                <img
+                                    src={getImage(
+                                        troop.equipment?.[0].name || ""
+                                    )}
+                                    className="hero-item"
+                                    alt=""
+                                />
+                                <img
+                                    src={getImage(
+                                        troop.equipment?.[1].name || ""
+                                    )}
+                                    className="hero-item"
+                                    alt=""
+                                />
+                            </div>
+                            )}
                         </div>
                     ))}
                 </div>
             </div>
+            )}
             <div className="troop-section">
                 <h3>TROOPS</h3>
                 <div className="troops-container">
                     {base.troops.map((troop, index) => (
                         <div className="troop-container" key={index}>
-                            <h4 className={`troop-level ${troop.level === troop.maxLevel ? 'max-level' : ''}`}>{troop.level}</h4>
+                            <h4
+                                className={`troop-level ${
+                                    troop.level === troop.maxLevel
+                                        ? "max-level"
+                                        : ""
+                                }`}
+                            >
+                                {troop.level}
+                            </h4>
                             <img
                                 className="unit-img"
                                 src={getImage(troop.name)}
@@ -68,7 +104,15 @@ function PlayerHome({ base, trophies }) {
                     <div className="troops-container">
                         {base.supers.map((troop, index) => (
                             <div className="troop-container" key={index}>
-                                <h4 className={`troop-level ${troop.level === troop.maxLevel ? 'max-level' : ''}`}>{troop.level}</h4>
+                                <h4
+                                    className={`troop-level ${
+                                        troop.level === troop.maxLevel
+                                            ? "max-level"
+                                            : ""
+                                    }`}
+                                >
+                                    {troop.level}
+                                </h4>
                                 <img
                                     className="unit-img"
                                     src={getImage(troop.name)}
@@ -85,7 +129,15 @@ function PlayerHome({ base, trophies }) {
                     <div className="troops-container">
                         {base.sieges.map((troop, index) => (
                             <div className="troop-container" key={index}>
-                                <h4 className={`troop-level ${troop.level === troop.maxLevel ? 'max-level' : ''}`}>{troop.level}</h4>
+                                <h4
+                                    className={`troop-level ${
+                                        troop.level === troop.maxLevel
+                                            ? "max-level"
+                                            : ""
+                                    }`}
+                                >
+                                    {troop.level}
+                                </h4>
                                 <img
                                     className="unit-img"
                                     src={getImage(troop.name)}
@@ -102,7 +154,15 @@ function PlayerHome({ base, trophies }) {
                     <div className="troops-container">
                         {base.spells.map((troop, index) => (
                             <div className="troop-container" key={index}>
-                                <h4 className={`troop-level ${troop.level === troop.maxLevel ? 'max-level' : ''}`}>{troop.level}</h4>
+                                <h4
+                                    className={`troop-level ${
+                                        troop.level === troop.maxLevel
+                                            ? "max-level"
+                                            : ""
+                                    }`}
+                                >
+                                    {troop.level}
+                                </h4>
                                 <img
                                     className="unit-img"
                                     src={getImage(troop.name)}
@@ -119,7 +179,15 @@ function PlayerHome({ base, trophies }) {
                     <div className="troops-container" id="player-home-pets">
                         {base.pets.map((troop, index) => (
                             <div className="troop-container" key={index}>
-                                <h4 className={`troop-level ${troop.level === troop.maxLevel ? 'max-level' : ''}`}>{troop.level}</h4>
+                                <h4
+                                    className={`troop-level ${
+                                        troop.level === troop.maxLevel
+                                            ? "max-level"
+                                            : ""
+                                    }`}
+                                >
+                                    {troop.level}
+                                </h4>
                                 <img
                                     className="unit-img"
                                     src={getImage(troop.name)}
@@ -136,7 +204,15 @@ function PlayerHome({ base, trophies }) {
                     <div className="troops-container" id="player-home-pets">
                         {base.heroEquipment.map((troop, index) => (
                             <div className="troop-container" key={index}>
-                                <h4 className={`troop-level ${troop.level === troop.maxLevel ? 'max-level' : ''}`}>{troop.level}</h4>
+                                <h4
+                                    className={`troop-level ${
+                                        troop.level === troop.maxLevel
+                                            ? "max-level"
+                                            : ""
+                                    }`}
+                                >
+                                    {troop.level}
+                                </h4>
                                 <img
                                     className="unit-img"
                                     src={getImage(troop.name)}
