e304b9b1b31559a03dfaa2f308f289b38dd6c0c3
josedrios <joserios0531@gmail.com>
2025-01-08 13:12:15 -0800

    Revised clan card styling, mainly the clan badge and clan role

--- src/Components/feature/PlayerClan.jsx
+++ src/Components/feature/PlayerClan.jsx
@@ -1,16 +1,24 @@
-function PlayerClan({playerClan}) {
+function PlayerClan({ playerClan }) {
     return (
         <div id="player-clan">
             <div id="player-clan-info">
                 <h3 id="player-clan-title">{playerClan.clanName}</h3>
-                <div>Role: <p>{playerClan.clanRole}</p></div>
-                <div>Donated: <p>{playerClan.troopsDonated}</p></div>
-                <div>Received: <p>{playerClan.troopsReceived}</p></div>
-                <div>War Stars: <p>{playerClan.warStars}</p></div>
+                <div>
+                    Donated: <p>{playerClan.troopsDonated}</p>
+                </div>
+                <div>
+                    Received: <p>{playerClan.troopsReceived}</p>
+                </div>
+                <div>
+                    War Stars: <p>{playerClan.warStars}</p>
+                </div>
             </div>
-            <img id="player-clan-badge" src={playerClan.clanBadge} alt="" />
+            <div id="player-clan-image">
+                <img id="player-clan-badge" src={playerClan.clanBadge} alt="" />
+                <div id="player-clan-role">{playerClan.clanRole}</div>
+            </div>
         </div>
-      )
-  }
-  
-  export default PlayerClan
+    );
+}
+
+export default PlayerClan;
