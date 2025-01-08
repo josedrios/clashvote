3219246bd5a7aabd04ea2e26cb29f082b66502fa
josedrios <joserios0531@gmail.com>
2025-01-07 16:32:26 -0800

    Added responsiveness to individual achievements

--- src/Components/feature/PlayerAchievmenets.jsx
+++ src/Components/feature/PlayerAchievmenets.jsx
@@ -41,8 +41,10 @@ function PlayerAchievements({ achievements }) {
                             </div>
                             {ach.stars === 3 ? (
                                 <div className="ach-progress ach-completion">
-                                    {ach.completionInfo}
-                                    <FaCheck className="ach-check"/>
+                                    <p className="ach-completion-note">
+                                        {ach.completionInfo}
+                                    </p>
+                                    <FaCheck className="ach-check" />
                                 </div>
                             ) : (
                                 <div className="ach-progress">
@@ -58,7 +60,10 @@ function PlayerAchievements({ achievements }) {
                                                     100
                                                 }%`,
                                             }}
-                                        />{" "}
+                                        />
+                                    </div>
+                                    <div className="ach-percentage">
+                                        {((ach.value / ach.target) * 100).toFixed(2)}%
                                     </div>
                                 </div>
                             )}
@@ -70,8 +75,65 @@ function PlayerAchievements({ achievements }) {
             <div className="ach-section">
                 <h3 className="base-ach-header">Builder Achievements</h3>
                 <div className="base-ach-container">
-                    {achievements.home.map((ach, index) => (
-                        <div></div>
+                {achievements.builder.map((ach, index) => (
+                        <div className="ach-container" key={index}>
+                            <div className="ach-stars">
+                                {Array.from(
+                                    { length: ach.stars },
+                                    (_, index) => (
+                                        <img
+                                            className="ach-star"
+                                            key={index}
+                                            src={getImage("star")}
+                                            alt=""
+                                        />
+                                    )
+                                )}
+                                {Array.from(
+                                    { length: 3 - ach.stars },
+                                    (_, index) => (
+                                        <img
+                                            className="ach-star-empty"
+                                            key={index}
+                                            src={getImage("star")}
+                                            alt=""
+                                        />
+                                    )
+                                )}
+                            </div>
+                            <div className="ach-body">
+                                <div className="ach-name">{ach.name}</div>
+                                <div className="ach-info">{ach.info}</div>
+                            </div>
+                            {ach.stars === 3 ? (
+                                <div className="ach-progress ach-completion">
+                                    <p className="ach-completion-note">
+                                        {ach.completionInfo}
+                                    </p>
+                                    <FaCheck className="ach-check" />
+                                </div>
+                            ) : (
+                                <div className="ach-progress">
+                                    <div className="ach-progress-text">
+                                        {ach.value}/{ach.target}
+                                    </div>
+                                    <div className="ach-progress-bar">
+                                        <div
+                                            className="ach-fill-bar"
+                                            style={{
+                                                width: `${
+                                                    (ach.value / ach.target) *
+                                                    100
+                                                }%`,
+                                            }}
+                                        />
+                                    </div>
+                                    <div className="ach-percentage">
+                                        {((ach.value / ach.target) * 100).toFixed(2)}%
+                                    </div>
+                                </div>
+                            )}
+                        </div>
                     ))}
                 </div>
             </div>
