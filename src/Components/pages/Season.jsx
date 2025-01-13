07b32e9dcf02f7b401801a1a016a546f9ae2f972
josedrios <joserios0531@gmail.com>
2025-01-13 15:33:13 -0800

    Created the create comment section (front end only)

--- src/Components/pages/Season.jsx
+++ src/Components/pages/Season.jsx
@@ -17,10 +17,15 @@ function Season() {
 export default Season;
 
 function VoteComponent() {
+    const comments = [
+        ["Jose", "This is a testing comment"],
+        ["Eric", "I am testing the UI of my comment section"],
+        ["Anna", "Loving this new feature!"]
+    ];
     return (
         <div>
             <Graph object={Data.heroes} title={"Heroes"} />
-            <CommentSection />
+            <CommentSection comments={comments} />
         </div>
     );
 }
@@ -57,20 +62,33 @@ function Graph({ object, title }) {
     );
 }
 
-function CommentSection() {
+function CommentSection({ comments }) {
     return (
         <div className="comment-section">
             <div className="comments-container">
                 <h3>
                     Comments<p>(39)</p>
                 </h3>
-                <Comment />
+                {comments.map(([username, comment], index) => (
+                    <Comment
+                        username={username}
+                        comment={comment}
+                        key={index}
+                    />
+                ))}
+            </div>
+            <div className="create-comment">
+                <input className="comment-input-field" type="text" placeholder="Type your comment here..." />
+                <div className="create-comment-actions">
+                  <button className="cancel-comment-button">Cancel</button>
+                  <button className="create-comment-button">Comment</button>
+                </div>
             </div>
         </div>
     );
 }
 
-function Comment() {
+function Comment({ username, comment }) {
     return (
         <div className="comment">
             <div className="comment-pfp-container">
@@ -78,32 +96,27 @@ function Comment() {
             </div>
             <div className="comment-info">
                 <div className="comment-top-bar">
-                    <h4 className="comment-writer">UsernameGoesHere</h4>
+                    <h4 className="comment-writer">{username}</h4>
                     <button className="comment-misc-button">
                         <FaEllipsisVertical className="ellipsis-icon" />
                     </button>
                 </div>
-                <div className="comment-content">
-                    This comment is a fake comment to test the UI of my comment
-                    section!?
-                </div>
+                <div className="comment-content">{comment}</div>
                 <div className="comment-actions">
                     <div className="react-button-count">
                         <button className="comment-reaction-button like-button">
                             <FaArrowUp />
                             <p>617</p>
                         </button>
-                  
                     </div>
                     <div className="react-button-count">
                         <button className="comment-reaction-button dislike-button">
                             <FaArrowDown />
                             <p>13</p>
                         </button>
-                        
                     </div>
                     <button className="comment-reply-button">
-                        <MdOutlineChat className="reply-icon"/>
+                        <MdOutlineChat className="reply-icon" />
                         <p>Reply</p>
                     </button>
                 </div>
