21b7b6d4c3302b9a6893c49dbb7acde86637fc99
josedrios <joserios0531@gmail.com>
2025-01-16 10:42:45 -0800

    Made revisions to the about page's structure and styling, removed the json and just moved the data directly to the about page jsx file

--- src/Components/pages/About.jsx
+++ src/Components/pages/About.jsx
@@ -1,10 +1,18 @@
 import images from "../feature/Images";
-import AboutInfo from "../../about.json";
-
 const getImage = (name) => images[name.replace(/[ .]/g, "_")] || null;
 
 function About({}) {
-    const pfp = ["Archer", "Golem", "Wizard", "Thrower", "Inferno Dragon"];
+    const pfp = ["Archer", "Golem", "Wizard", "Thrower", "Inferno Dragon", "Valkyrie"];
+    const aboutP1 = "I started playing Clash of Clans (COC) as a kid, over a decade ago, and watched it evolve while building my base. I played with my brothers and friends but always felt a lack of community. The removal of global chat was a major blow, and though I kept playing, the sense of connection was never the same."
+    const aboutP2 = "Now, as a young adult with programming skills, I want to give back to the same community that shaped me into the man I am today by making them a helpful website. This platform will allow players to vote on various topics, share guidance, and discuss gameplay concepts, fostering collaboration and helping everyone navigate the game better."
+    const roles = [
+        "Front End Engineer",
+        "Back End Engineer",
+        "UI/UX Designer",
+        "DevOps Engineer",
+        "Database Administrator",
+        "QA Engineer"
+    ]
 
     return (
         <div id="about-container">
@@ -12,9 +20,12 @@ function About({}) {
                 Meet the <p>Team</p>
             </h3>
             <div id="about-us-body">
-                <div id="about-us-description">{AboutInfo.about}</div>
+                <div id="about-us-description">
+                    <div className="desc-paragraph">&emsp;{aboutP1}</div>
+                    <div className="desc-paragraph">&emsp;{aboutP2}</div>
+                </div>
                 <div id="team-member-container">
-                    {AboutInfo.team.map((dev, index) => (
+                    {roles.map((role, index) => (
                         <div className="team-member-card" key={index}>
                             <img
                                 className="team-member-pfp"
@@ -26,7 +37,7 @@ function About({}) {
                                     Jose D. Rios
                                 </div>
                                 <div className="team-member-role">
-                                    {dev.role}
+                                    {role}
                                 </div>
                             </div>
                         </div>
