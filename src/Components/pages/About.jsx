accf48f12074df5f0c7176de19b18082b54be3f2
josedrios <joserios0531@gmail.com>
2025-01-16 13:39:33 -0800

    Completed the contact page, structuring and styling, functionality for the form is still left to do

--- src/Components/pages/About.jsx
+++ src/Components/pages/About.jsx
@@ -4,7 +4,7 @@ const getImage = (name) => images[name.replace(/[ .]/g, "_")] || null;
 function About({}) {
     const pfp = ["Archer", "Golem", "Wizard", "Thrower", "Inferno Dragon", "Valkyrie"];
     const aboutP1 = "I started playing Clash of Clans (COC) as a kid, over a decade ago, and watched it evolve while building my base. I played with my brothers and friends but always felt a lack of community. The removal of global chat was a major blow, and though I kept playing, the sense of connection was never the same."
-    const aboutP2 = "Now, as a young adult with programming skills, I want to give back to the same community that shaped me into the man I am today by making them a helpful website. This platform will allow players to vote on various topics, share guidance, and discuss gameplay concepts, fostering collaboration and helping everyone navigate the game better."
+    const aboutP2 = "Now, as a young adult with programming skills, I want to give back to the same community that shaped me into the man I am today by making a helpful website. This platform will allow players to vote on various topics, share guidance, and discuss gameplay concepts, fostering collaboration and helping everyone navigate the game better."
     const roles = [
         "Front End Engineer",
         "Back End Engineer",
--- src/Components/pages/Contact.jsx
+++ src/Components/pages/Contact.jsx
@@ -1,34 +1,55 @@
+import { MdOutlineEmail } from "react-icons/md";
+import { FaDiscord } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+
 function Contact({}) {
     return (
         <div id="contact-container">
-            <div id="contact-info">
-                <h3>Contact Info</h3>
-                <p>Need to get in touch? Shoot us an email</p>
-                <div>clashvote@example.com</div>
-            </div>
-            <div id="contact-create-message">
-                <form action="">
-                    <div id="contact-naming-container">
-                        <div
-                            id="first-name-section"
-                            className="contact-name-section"
-                        >
-                            <h4>First Name:</h4>
-                            <input type="text" placeholder="First" />
-                        </div>
-                        <div
-                            id="last-name-section"
-                            className="contact-name-section"
-                        >
-                            <h4>Last Name:</h4>
-                            <input type="text" placeholder="Last" />
-                        </div>
+            <h2 id="contact-title">Contact Us</h2>
+            <div id="contact-body-container">
+                <div id="contact-info">
+                    <h3>Have questions or suggestions?</h3>
+                    <div>clashvote@gmail.com</div>
+                    <div id="contact-link-buttons">
+                        <button>
+                            <MdOutlineEmail />
+                        </button>
+                        <button>
+                            <FaDiscord />
+                        </button>
+                        <button>
+                            <FaInstagram />
+                        </button>
                     </div>
-                    <h4>Email:</h4>
-                    <input id="contact-email-input" type="text" placeholder="Enter your email here..." />
-                    <h4>Message:</h4>
-                    <textarea name="" id="contact-message-text"></textarea>
-                </form>
+                </div>
+                <div id="contact-create-message">
+                    <form action="">
+                        <div id="contact-naming-container">
+                            <div
+                                id="first-name-section"
+                                className="contact-name-section"
+                            >
+                                <h4>First Name:</h4>
+                                <input type="text" placeholder="First" />
+                            </div>
+                            <div
+                                id="last-name-section"
+                                className="contact-name-section"
+                            >
+                                <h4>Last Name:</h4>
+                                <input type="text" placeholder="Last" />
+                            </div>
+                        </div>
+                        <h4>Email:</h4>
+                        <input
+                            id="contact-email-input"
+                            type="text"
+                            placeholder="Enter your email here..."
+                        />
+                        <h4>Message:</h4>
+                        <textarea name="" id="contact-message-text"></textarea>
+                    </form>
+                </div>
             </div>
         </div>
     );
