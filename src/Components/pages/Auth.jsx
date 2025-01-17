7a11f9a78bbc7e60a1c47ca126471f4203052c71
josedrios <joserios0531@gmail.com>
2025-01-17 15:26:40 -0800

    Was able to get the auth tab slider logic too work (smooth sliding transition still needs to be coded)

--- src/Components/pages/Auth.jsx
+++ src/Components/pages/Auth.jsx
@@ -2,21 +2,25 @@ import images from "../../Components/feature/Images";
 import { FiUser } from "react-icons/fi";
 import { HiOutlineMail } from "react-icons/hi";
 import { MdLockOutline } from "react-icons/md";
+import { useState } from "react";
 
 const getImage = (name) => images[name.replace(/[ .]/g, "_")] || null;
 
 function Auth({}) {
+    const[authTab, setAuthTab] = useState("login")
+
     return (
         <div id="auth-container">
             <div id="sign-log-container">
                 <div id="signup-container">
                     <h3 className="sign-log-header">Sign Up</h3>
+                    <AuthForm authType={"signup"} setAuthTab={setAuthTab}/>
                 </div>
                 <div id="login-container">
                     <h3 className="sign-log-header">Log In</h3>
-                    <AuthForm authType={"login"} />
+                    <AuthForm authType={"login"} setAuthTab={setAuthTab}/>
                 </div>
-                <div id="art-slider">
+                <div id="art-slider" className={authTab === "login" ? "": "art-slide-right"}>
                     <img src={getImage("slider")} alt="" />
                 </div>
             </div>
@@ -26,9 +30,9 @@ function Auth({}) {
 
 export default Auth;
 
-function AuthForm({ authType }) {
+function AuthForm({ authType, setAuthTab }) {
     return (
-        <form id="auth-form" action="">
+        <form className="auth-form" action="" id={`${authType}-form`}>
             <div className="auth-input-container">
                 <FiUser className="auth-input-icon" />
                 <input type="text" name="username" placeholder="Username" />
@@ -42,12 +46,27 @@ function AuthForm({ authType }) {
                 <input type="password" name="username" placeholder="Password" />
             </div>
             <div id="auth-tos-container">
-                <p><input type="checkbox" name="tos" id="auth-form-tos" /> I agree
-                to the <a href="">terms of services</a></p>
+                <p>
+                    <input type="checkbox" name="tos" id="auth-form-tos" /> I
+                    agree to the <a href="">terms of services</a>
+                </p>
             </div>
             <button id="auth-form-confirm">{authType.toUpperCase()}</button>
-            <a className="forgot-already-link" id="already-link" href="">Already have an account?</a>
-            <a className="forgot-already-link" id="forgot-link" href="">Forgot password?</a>
+            {authType === "signup" && (
+                <a className="forgot-already-link" id="already-link" href="" onClick={(e) => {e.preventDefault(); setAuthTab("login");}}>
+                    Already have an account?
+                </a>
+            )}
+            {authType === "login" && (
+                <>
+                    <a className="forgot-already-link" id="forgot-link" href="" onClick={(e) => {e.preventDefault(); setAuthTab("signup");}}>
+                        Don't have an account?
+                    </a>
+                    <a className="forgot-already-link" id="forgot-link" href="">
+                        Forgot password?
+                    </a>
+                </>
+            )}
         </form>
     );
 }
